438232ba7d58ad8477f77e9c3a20124168f923a0
Ubuntu <myong@ParallelTDA.nmfry343pmaedbqbbulyqv1isd.zx.internal.cloudapp.net>
2018-03-27 13:00:28 +0000

    Converted to off files

---
+++ data_sets/roadmap_datasets_point_cloud/offconverter.sh
@@ -0,0 +1,59 @@
+filename="dragon_vrip.ply.txt_1000_.txt"
+sed -i -e '1i1000 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="dragon_vrip.ply.txt_2000_.txt"
+sed -i -e '1i2000 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="fractal_9_5_2_linear_edge_list.txt_1866.1116_point_cloud.txt"
+sed -i -e '1i512 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="fractal_9_5_2_random_edge_list.txt_0.19795_point_cloud.txt"
+sed -i -e '1i512 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="fractal_9_5_2_weight_one_edge_list.txt_2_point_cloud.txt"
+sed -i -e '1i516 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="H3N2.all.nt.concat.fa_hdm.txt_point_cloud.txt"
+sed -i -e '1i2722 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="HIV1_2011.all.nt.concat.fa_hdm.txt_point_cloud.txt"
+sed -i -e '1i1088 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="house104_edge_list.txt_0.72344_point_cloud.txt"
+sed -i -e '1i445 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="human_gene2_sampled_reindexed_for_matlab.txt_maxdist_91.9097_SP_distmat.txt_point_cloud.txt"
+sed -i -e '1i1397 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="klein_bottle_pointcloud_new_400.txt"
+sed -i -e '1i400 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="klein_bottle_pointcloud_new_900.txt"
+sed -i -e '1i900 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="network379_edge_list.txt_38.3873_point_cloud.txt"
+sed -i -e '1i379 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="random_point_cloud_1000_8_.txt"
+sed -i -e '1i1000 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="random_point_cloud_50_16_.txt"
+sed -i -e '1i50 0 0\' $filename
+sed -i -e '1iOFF\' $filename
+
+filename="network379_edge_list.txt_38.3873_point_cloud.txt"
+sed -i -e '1i379 0 0\' $filename
+sed -i -e '1iOFF\' $filename
